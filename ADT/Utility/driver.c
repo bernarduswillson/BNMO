37bd7ab6f0a89b464198c56605ded8e889fcf95b
unknown <16521486@mahasiswa.itb.ac.id>
2022-10-27 19:28:57 +0700

    finishing inisiasi

--- ADT/Utility/driver.c
+++ ADT/Utility/driver.c
@@ -1,6 +1,10 @@
 #include "inisiasi.c"
+#include "../Mesin_Kata/wordmachine.c"
+#include "../Mesin_Karakter/charmachine.c"
 
 int main() {
     startMenu();
+    STARTWORD();
+    checkInput(currentWord);
     return 0;
 }
